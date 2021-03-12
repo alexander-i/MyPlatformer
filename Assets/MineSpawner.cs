5f5fd3bad22eaa2e59015fd0c5b15e9b5401ee42
Alexander Ivanov <al.m.ivanov@gmail.com>
2021-03-12 15:54:59 +0300

    lesson 2

---
+++ Assets/MineSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSpawner : MonoBehaviour
+{
+    [SerializeField] private Mine _mine;
+    [SerializeField] private Transform _mineSpawnPlaceLeft;
+    [SerializeField] private Transform _mineSpawnPlaceRight;
+    private Transform _mineSpawnPlace;
+
+    private void Awake()
+    {
+        _mineSpawnPlace = _mineSpawnPlaceRight;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _mine.Move(_mineSpawnPlace.position);
+        }
+    }
+
+    public void SetDirection(bool right)
+    {
+        if (right)
+        {
+            _mineSpawnPlace = _mineSpawnPlaceRight;
+        }
+        else
+        {
+            _mineSpawnPlace = _mineSpawnPlaceLeft;
+        }
+    }
+}
---
+++ Assets/MyEnemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MyEnemy : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private float _speed;
+    [SerializeField] private GameObject _enemyObject;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private int _detectionRange;
+    private Vector3 _position;
+    public bool facingRight = true;
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(_player.transform.position.x - _position.x) > _detectionRange || Mathf.Abs(_player.transform.position.x - _position.x) <= 3)
+        {
+
+        }
+        else if (_player.transform.position.x < _position.x)
+        {
+            _position.x -= _speed;
+            if (facingRight) { Flip(); }
+        }
+        else if (_player.transform.position.x > _position.x)
+        {
+            _position.x += _speed;
+            if (!facingRight) { Flip(); }
+        }
+        
+        transform.position = _position;
+    }
+
+    public void Initalize(Vector3 newPosition)
+    {
+        _position = newPosition;
+        _enemyObject.SetActive(true);
+    }
+
+    public void Hurt(int damage)
+    {
+        print("Ouch: " + damage);
+
+        _health -= damage; ;
+
+        if (_health <= 0)
+        {
+            Die();
+            transform.parent.GetComponent<EnemySpawner>().Initialize();
+        }
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void Die()
+    {
+        _enemyObject.SetActive(false);
+    }
+}
